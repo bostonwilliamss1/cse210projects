5274381076b1bb6df7c3256af4845aefd5eb4237
bostonwilliamss1 <bostonwilliamss1@gmail.com>
2023-11-27 16:54:12 -0500

    Prepare activity:)))

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("blue", 6);
+
+        Rectangle r1 = new Rectangle(3, 4, "green");
+
+        Circle c1 = new Circle("orange", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(r1);
+        shapes.Add(s1);
+        shapes.Add(c1);
+        
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine(area);
+            Console.WriteLine(color);
+        }
+        
     }
 }
